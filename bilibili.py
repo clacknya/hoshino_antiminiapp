5b5378e8d1b941dc1ce6f54835d24d915d05613b
clacknya <clacknya@localhost>
2022-05-30 02:16:06 +0800

    update

--- bilibili.py
+++ bilibili.py
@@ -5,11 +5,14 @@ from typing import Text, List, Dict
 
 import aiohttp
 import re
+import datetime
 
 from hoshino.typing import MessageSegment
 
 pattern_id = re.compile(r'\bbilibili\.com\/video\/(?:av(?P<aid>[0-9]+)|(?P<bvid>bv[0-9a-zA-Z]+))', flags=re.I)
 pattern_url = re.compile(r'\bbilibili\.com\/video\/(?:av[0-9]+|bv[0-9a-zA-Z]+)|\bb23\.tv\/[a-zA-Z0-9]+', flags=re.I)
+
+tz = datetime.timezone(datetime.timedelta(hours=+8))
 
 async def get_info(url: Text) -> Text:
 	async with aiohttp.ClientSession(raise_for_status=True) as session:
@@ -21,10 +24,11 @@ async def get_info(url: Text) -> Text:
 			ret = await resp.json()
 	data = ret['data']
 	return (
-		f"{MessageSegment.image(data['pic'])}\n"
+		f"{MessageSegment.image(data['pic'])}"
 		f"av{data['aid']}\n"
 		f"{data['title']}\n"
 		f"UP:{data['owner']['name']}\n"
+		f"日期:{datetime.datetime.fromtimestamp(data['pubdate'], tz).strftime('%Y-%m-%d %H:%M:%S')}\n"
 		f"{data['stat']['view']:,}播放 {data['stat']['danmaku']:,}弹幕\n"
 		f"https://www.bilibili.com/video/{data['bvid']}"
 	)
