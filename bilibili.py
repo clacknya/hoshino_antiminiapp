d77eabaf7c866b1204e261eb8c1377f4edf960cd
clacknya <clacknya@localhost>
2022-05-31 03:49:07 +0800

    add code check

--- bilibili.py
+++ bilibili.py
@@ -15,22 +15,34 @@ pattern_url = re.compile(r'\bbilibili\.com\/(?:video\/)?(?:av[0-9]+|bv[0-9a-zA-Z
 tz = datetime.timezone(datetime.timedelta(hours=+8))
 
 async def get_info(url: Text) -> Text:
-	async with aiohttp.ClientSession(raise_for_status=True) as session:
+	async with aiohttp.ClientSession(raise_for_status=False) as session:
 		async with session.head(url, allow_redirects=True) as resp:
 			url = str(resp.url)
 		params = {k: v for k, v in pattern_id.search(url).groupdict().items() if v}
 		async with session.get('https://api.bilibili.com/x/web-interface/view', params=params) as resp:
 			ret = await resp.json()
-	data = ret['data']
-	return (
-		f"{MessageSegment.image(data['pic'])}"
-		f"av{data['aid']}\n"
-		f"{data['title']}\n"
-		f"UP:{data['owner']['name']}\n"
-		f"日期:{datetime.datetime.fromtimestamp(data['pubdate'], tz).strftime('%Y-%m-%d %H:%M:%S')}\n"
-		f"{data['stat']['view']:,}播放 {data['stat']['danmaku']:,}弹幕\n"
-		f"https://www.bilibili.com/video/{data['bvid']}"
-	)
+	if ret['code'] == 0:
+		data = ret['data']
+		return (
+			f"{MessageSegment.image(data['pic'])}"
+			f"av{data['aid']}\n"
+			f"{data['title']}\n"
+			f"UP:{data['owner']['name']}\n"
+			f"日期:{datetime.datetime.fromtimestamp(data['pubdate'], tz).strftime('%Y-%m-%d %H:%M:%S')}\n"
+			f"{data['stat']['view']:,}播放 {data['stat']['danmaku']:,}弹幕\n"
+			f"https://www.bilibili.com/video/{data['bvid']}"
+		)
+	elif ret['code'] == -404:
+		return (
+			f"{MessageSegment.image('https://s1.hdslb.com/bfs/static/jinkela/video/asserts/no_video.png')}"
+			f"啊叻？视频不见了？"
+		)
+	elif ret['code'] == -400:
+		return (
+			f"{MessageSegment.image('https://static.hdslb.com/error/very_sorry.png')}"
+		)
+	else:
+		return f"code {ret['code']}: {ret['message']}"
 
 def parse_urls(text: Text) -> List[Text]:
 	return list(map(lambda x: 'https://' + x, set(pattern_url.findall(text))))
